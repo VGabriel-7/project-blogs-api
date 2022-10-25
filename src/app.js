15e4fe6242c2d34da57a323362539ab9da577251
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-10-25 14:38:30 -0300

    Iniciando projeto :rocket:

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 
 // ...
 
-// É importante exportar a constante `app`,
+// É importante exportar a constante `app`
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
